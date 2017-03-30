cf122a4134f008e76da3f627f7dd63aa7d948e3f
Jamey Fraser <jfrase09@uoguelph.ca>
2017-03-30 15:15:33 -0400

    Deleted unused fileuploadController. Moved Survey Questions to own folder. Adding Testing Routes for diffent types of data.

--- ifs/app/components/Test/testRoutes.js
+++ ifs/app/components/Test/testRoutes.js
@@ -5,7 +5,27 @@ var _ = require('lodash');
 
 module.exports = function( app ) {
 
-    app.get('/', function(req,res, next ){
-        res.render( viewPath + "test", { "title":"hellow" });
+    function prettyJson( filename ) {
+        var j = fs.readFileSync( filename, 'utf-8');
+        j = JSON.parse(j);
+        j = JSON.stringify(j,null,4);
+        return j;
+    }
+
+    app.get('/jobRequests', function(req,res, next ){
+
+        var j = "No contents available or incorrect file name";
+        if(  req.session.jobRequestFile ) {
+            j = prettyJson( req.session.jobRequestFile );
+        }
+        res.render( viewPath + "test", { "title":"Your Job Requets Data", "values":j });
+    });
+
+    app.get('/rawFeedback', function(req,res, next ){
+       var j = "No contents available or incorrect file name";
+        if(  req.session.allFeedbackFile ) {
+            j = prettyJson( req.session.allFeedbackFile );
+        }
+        res.render( viewPath + "test", { "title":"Your Raw Feedback", "values":j });
     });
 };
