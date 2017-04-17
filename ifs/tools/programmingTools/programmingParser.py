bb488ae60e9d01292e667734c72f3719a905444f
Jamey Fraser <jfrase09@uoguelph.ca>
2017-04-16 23:47:57 -0400

    Left print statement in programmer parser will cause no result error

--- ifs/tools/programmingTools/programmingParser.py
+++ ifs/tools/programmingTools/programmingParser.py
@@ -231,8 +231,6 @@ def main(argv):
                     errors = errFile.read()
                     result = parse( errors, options )
 
-                    print(result)
-
                     if( options['ifs'] ):
                         result = decorateData( result, options )
                     print( result )
