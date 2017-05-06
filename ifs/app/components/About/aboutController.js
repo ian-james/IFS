1a9ac73beb537d51ea5544ed990406dcf0a43418
Jamey Fraser <jfrase09@uoguelph.ca>
2017-05-05 20:16:50 -0400

    Adding some additional help for about page.

--- ifs/app/components/About/aboutController.js
+++ ifs/app/components/About/aboutController.js
@@ -1,6 +1,11 @@
 app.controller("aboutCtrl", function($scope, $http) {
+
+
     $scope.toolAbtList = [];
-    $http.get('/about').then(function(res) {
+    $http.get('/about/data').then(function(res) {
+
+        console.log(res);
+        // Res.data contains two variables for each prog and lang files.
         $scope.toolAbtList = res.data;
         console.log($scope.toolAbtList); // this just prints the page source??
     });
--- ifs/app/components/About/aboutRoutes.js
+++ ifs/app/components/About/aboutRoutes.js
@@ -9,32 +9,51 @@ module.exports = function( app ) {
 
     .get( function(req,res,next){
         res.render( viewPath + "about", { title: 'About page', message:'ok'})
-    })
+    });
 
     // this is not working
-    app.get('/about', function(req,res) {
+    app.get('/about/data', function(req,res) {
         var langToolsFile = './tools/toolList.json';
         var progToolsFile = './tools/toolListProgramming.json';
-        fs.readFile(langToolsFile, 'utf-8', function(err, data) {
-            if (err) {
-                // unable to get supported tools file, larger problem here.
-                console.log(err);
-            } else {
-                var langToolsObj = JSON.parse(data);
-            }
-        });
-        fs.readFile(progToolsFile, 'utf-8', function(err, data) {
-            if (err) {
-                // unable to get supported tools file, larger problem here.
-                console.log(err);
-            } else {
-                var progToolsObj = JSON.parse(data);
-            }
 
+        console.log("ACTUALLY CALLING app.get ");
+
+        // Callbacks in Node are slightly differnt that every other language (probalby?!)
+        // They aren't good for returning values, they are mostly good for accomplishing something
+        //
+        // So in English, it's read langToolsFile finished
+        // Read progToolsFile
+        // When Done
+        // Check for an error in either (Note...nothing really handled there)
+        // If everything was ok, we should have file data for both.
+        // Pase the data and send to Angular controlller
+        // res.json actually finishes teh request, i believe.
+        fs.readFile(langToolsFile, 'utf-8', function(errLang, langToolData) {
+
+            fs.readFile(progToolsFile, 'utf-8', function(errProg, progToolData) {
+                if (errLang || errProg ) {
+                    // unable to get supported tools file, larger problem here.
+                    // Unlikely to occur given these are static files but never know.
+                    console.log(err);
+
+                    // You might want to send empty array here, but about.pug will need to check for empty values then.
+                    // Up to you how you handle this.
+                    // res.json('lang'[], 'prog':[]})
+                } else {
+                    var langToolsObj = JSON.parse(langToolData);
+                    var progToolsObj = JSON.parse(progToolData);
+
+                    console.log("lang", langToolsObj);
+                    console.log("prog", progToolsObj);
+
+                    res.json({'lang':langToolsObj, 'prog':progToolsObj})
+                }
+            });
         });
 
-        console.log('where does this log to??');
-        res.json({'lang':langToolsObj, 'prog':progToolsObj})
+        // This will log before readFile has finished in most cases.
+        console.log('This logs to server command window');
+        //Logger.info("Could also work here if Logger is included via require");
     })
 }
 
