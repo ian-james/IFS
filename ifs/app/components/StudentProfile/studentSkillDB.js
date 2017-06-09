f818c37a289d7f8dad2595764ffcf817791fcbe4
Keefer Rourke <mail@krourke.org>
2017-06-09 15:01:19 -0400

    fixed apparent mysql syntax error

--- ifs/app/components/StudentProfile/studentSkillDB.js
+++ ifs/app/components/StudentProfile/studentSkillDB.js
@@ -6,12 +6,12 @@ var dbHelpers = require(__components + "Databases/dbHelpers");
 module.exports = {
 
     getStudentSkills: function( studentId, callback ) {
-        var q = "select cs.name,cs.description,ss.value from student_skill ss, class_skill cs where ss.studentId = ? and cs.id = ss.classSkillId";
+        var q = "SELECT cs.name, cs.description, ss.value FROM student_skill ss, class_skill cs WHERE ss.studentId = ? AND cs.id = ss.classSkillId";
         db.query(q,studentId,callback);
     },
 
     getStudentTop3Skills: function( studentId,callback ) {
-        var q = "select cs.name, cs.description, ss.value from student_skill ss, class_skill cs where ss.studentId = ? and cs.id = ss.classSkillId ORDER BY value desc LIMIT 3";
+        var q = "SELECT cs.name, cs.description, ss.value from student_skill ss, class_skill cs WHERE ss.studentId = 8 AND cs.id = ss.classSkillId ORDER BY value LIMIT 3;"
         db.query(q,studentId,callback)
     }
 }
