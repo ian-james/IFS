0bcfa0b46694aaa3b4095a8062a7e5a4b3b61ea1
Keefer Rourke <mail@krourke.org>
2017-08-28 09:48:49 -0500

    bump to v0.0.2

--- ifs/config/version.js
+++ ifs/config/version.js
@@ -1,6 +1,6 @@
 // this version number should stay in sync with the number reported by
 // `npm version`
-var version = "0.0.1";
+var version = "0.0.2";
 
 // show the version number at the footer of every page
 var vstr = "IFS v" + version;
