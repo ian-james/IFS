1dca529ab3b739ef29e5405cb80a5c0e92d1461c
Jamey Fraser <jfrase09@uoguelph.ca>
2017-09-03 22:21:52 -0400

    Adding skill meter fix tried to make jquery more resistant to relative layout changes. Put Focus back on top

--- ifs/app/components/StudentSkill/studentSkillSliders.js
+++ ifs/app/components/StudentSkill/studentSkillSliders.js
@@ -1,16 +1,21 @@
 $(function(){
     $('.studentInput').on('change', function() {
-        var sVal = Math.min(100, Math.max(0, $(this).val()));
-        var parent = $(this).parent();
 
-        var pnext = parent.next();
-        var progress = pnext.children('progress');
+        //constants relating to studentSkill.pug
+        var inputName = "numeric";
+        var progressName = "prog"
+        var hiddenName = "userHidden";
+
+        var sVal = Math.min(100, Math.max(0, $(this).val()));
+        var inputId = $(this).attr('id');
+        var idVal = inputId.substr( inputName.length );
+
+        var progress = $('#'+ progressName + idVal );
 
         if(progress) {
             // Change the progress bar.
             progress.val(sVal);
-            var hidden = pnext.next();
-
+            var hidden = $('#' + hiddenName + idVal);
             if(hidden) {
                 // Set hidden to have changed to track.
                 hidden.val('yes');
