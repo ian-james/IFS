1940089fffb63495d5ef946c2fcf6885910da152
Keefer Rourke <mail@krourke.org>
2017-09-24 10:30:13 -0400

    patch admin dashboard inputs

--- ifs/app/components/Admin/adminRoutes.js
+++ ifs/app/components/Admin/adminRoutes.js
@@ -105,48 +105,33 @@ module.exports = function( app ) {
         }
     }
 
-    function validateTitles(res, route, titles) {
+    function validateTitles(res, route, titles, cb) {
         var error = false
         for (var i = 0; i < titles.length; i++) {
             if (!sanitization.validateText(titles[i], 'title')) {
                 error = true;
-                directTo(res, url.format({
-                    pathname: route,
-                    query: {
-                        err: 'title'
-                    }
-                }));
+                break;
             }
         }
-        return error;
+        cb(error, route);
     }
-    function validateDesc(res, route, desc) {
+    function validateDesc(res, route, desc, cb) {
         var error = false
         if (!sanitization.validateText(desc, 'par')) {
             error = true;
-            directTo(res, url.format({
-                pathname: route,
-                query: {
-                    err: 'desc'
-                }
-            }));
         }
-        return error;
+        cb(error, route);
     }
-    function validateDates(res, route, dates) {
+    function validateDates(res, route, dates, cb) {
         var error = false;
         for (var i = 0; i < dates.length; i++) {
             if (!validator.toDate(dates[i])) {
                 error = true;
-                directTo(res, url.format({
-                    pathname: route,
-                    query: {
-                        err: 'date'
-                    }
-                }));
+                break;
             }
+
         }
-        return error;
+        cb(error, route);
     }
 
     /**
@@ -198,23 +183,41 @@ module.exports = function( app ) {
     })
     .post(function(req,res,next) {
         var route = '/admin-add-course'
-        var error = false;
         var code = req.body['class-code']
         var name = req.body['class-name']
         var desc = req.body['class-description']
 
-        error = validateTitles(res, route, [code, name]);
-        if (!error)
-            error = validateDesc(res, route, desc);
+        validateTitles(res, route, [code, name], function(err, route) {
+           if (err) { 
+                directTo(res, url.format({
+                    pathname: route,
+                    query: {
+                        err: 'title'
+                    }
+                }));
+            } else {
+                error = validateDesc(res, route, desc, function(err2, route) {
+                    if (err2) {
+                        directTo(res, url.format({
+                            pathname: route,
+                            query: {
+                                err: 'desc'
+                            }
+                        }));
+                    } else {
+                        var courseKeys = ["class-code","class-name","class-description","class-disciplineType"];
 
-        var courseKeys = ["class-code","class-name","class-description","class-disciplineType"];
+                        var submission = _.pick(req.body, courseKeys);
+                        var values = _.values(submission);
 
-        var submission = _.pick(req.body, courseKeys);
-        var values = _.values(submission);
-
-        adminDB.insertCourse(values, function(err,result){
-            directTo(res);
+                        adminDB.insertCourse(values, function(err,result){
+                            directTo(res);
+                        });
+                    }
+                });
+            }
         });
+
     });
 
     /*************************************************************ADMIN Add Event**/
@@ -242,26 +245,51 @@ module.exports = function( app ) {
         var title = req.body['event-title']
         var desc = req.body['event-description']
 
-        error = validateTitles(res, route, title);
-        if (!error)
-            error = validateDesc(res, route, desc);
-        if (!error)
-            error = validateDates(res, route, [req.body['event-startDate'], req.body['event-endDatee']]);
+        validateTitles(res, route, [title, name], function(err, route) {
+            if (err) {
+                directTo(res, url.format({
+                    pathname: route,
+                    query: {
+                        err: 'title'
+                    }
+                }));
+            } else {
+                validateDesc(res, route, desc, function(err2, route) {
+                    if (err2) {
+                        directTo(res, url.format({
+                            pathname: route,
+                            query: {
+                                err: 'desc'
+                            }
+                        }));
+                    } else {
+                        validateDates(res, route, [req.body['event-startDate'], req.body['event-endDate']], function(err3, route) {
+                            if (err3) {
+                                directTo(res, url.format({
+                                    pathname: route,
+                                    query: {
+                                        err: 'date'
+                                    }
+                                }));
+                            } else {
+                                var keys = ["event-name","event-title","event-description","event-startDate","event-endDate"];
+                                var submission = _.pick(req.body, keys);
+                                // Find the class id then insert event for class
+                                adminDB.getClassByCode(req.body['class-name'], function(err,data) {
 
-        if (!error) {
-            var keys = ["event-name","event-title","event-description","event-startDate","event-endDate"];
-            var submission = _.pick(req.body, keys);
-            // Find the class id then insert event for class
-            adminDB.getClassByCode(req.body['class-name'], function(err,data) {
+                                    var values = _.values(submission);
+                                    values.unshift(data[0].id);
 
-                var values = _.values(submission);
-                values.unshift(data[0].id);
-
-                adminDB.insertUpcomingEvent(values, function(err,result) {
-                    directTo(res);
+                                    adminDB.insertUpcomingEvent(values, function(err,result) {
+                                        directTo(res);
+                                    });
+                                });
+                            }
+                        });
+                    }
                 });
-            });
-        }
+            }
+        });
     });
 
     /**************************************************************ADMIN Add Assignment**/
@@ -290,30 +318,53 @@ module.exports = function( app ) {
         var desc= req.body['assignment-description'];
         var date= req.body['assignment-dueDate'];
 
-        error = validateTitles(res, route, [name, title]);
+        validateTitles(res, route, [name, title], function(err, route) {
+            if (err) {
+                directTo(res, url.format({
+                    pathname: route,
+                    query: {
+                        err: 'title'
+                    }
+                }));
+            } else {
+                validateDesc(res, route, desc, function(err2, route) {
+                    if (err2) {
+                        directTo(res, url.format({
+                            pathname: route,
+                            query: {
+                                err: 'desc'
+                            }
+                        }));
+                    } else {
+                        validateDates(res, route, [date], function(err3, route) {
+                            if (err3) {
+                                directTo(res, url.format({
+                                    pathname: route,
+                                    query: {
+                                        err: 'date'
+                                    }
+                                }));
+                            } else {
+                                var keys = ["assignment-name","assignment-title","assignment-description",
+                                            "assignment-dueDate"];
 
-        if (!error)
-            validateDesc(res, route, desc);
+                                var submission = _.pick(req.body, keys);
 
-        if (!error)
-            validateDates(res, route, [date]);
+                                // Find the class id then insert event for class
+                                adminDB.getClassByCode(req.body['class-name'], function(err,data) {
+                                    var values = _.values(submission);
+                                    values.unshift(data[0].id);
 
-        if (!error) {
-            var keys = ["assignment-name","assignment-title","assignment-description",
-                        "assignment-dueDate"];
-
-            var submission = _.pick(req.body, keys);
-
-            // Find the class id then insert event for class
-            adminDB.getClassByCode(req.body['class-name'], function(err,data) {
-                var values = _.values(submission);
-                values.unshift(data[0].id);
-
-                adminDB.insertAssignment(values, function(err,result) {
-                    directTo(res);
+                                    adminDB.insertAssignment(values, function(err,result) {
+                                        directTo(res);
+                                    });
+                                });
+                            }
+                        });
+                    }
                 });
-            });
-        }
+            }
+        });
     });
 
       /******************************************************************ADMIN Add Assignment Task**/
@@ -356,28 +407,45 @@ module.exports = function( app ) {
                     err: "a"
                 }
             }));
-        }
-        error = validateTitles(res, route, [name]);
-        if (!error)
-            error = validateDesc(res, route, desc);
-
-        if (!error) {
-            var keys = ['assignment-task-name','assignment-task-description'];
-
-            var submission = _.pick(req.body, keys);
-            var assignmentName = req.body['assignment-name'] == 'null' ? null : req.body['assignment-name'];
-            // Find the class id then insert event for class
-            adminDB.getAssignmentByClassCodeAndName(req.body['class-name'], assignmentName, function(err,data){
-                if(data && data.length > 0) {
-                    var values = _.values(submission);
-                    values.unshift(data[0].aId);
-
-                    adminDB.insertTask(values, function(err,result) {
-                        directTo(res);
-                    });
+        } else {
+            validateTitles(res, route, [name], function(err, route) {
+                if (err) {
+                    directTo(res, url.format({
+                        pathname: route,
+                        query: {
+                            err: 'title'
+                        }
+                    }));
                 } else {
-                    req.flash('errorMessage', 'Unable to add task, please check assignment belongs to class.');
-                    res.end();
+                    validateDesc(res, route, desc, function(err2, route) {
+                        if (err2) {
+                            directTo(res, url.format({
+                                pathname: route,
+                                query: {
+                                    err: 'desc'
+                                }
+                            }));
+                        } else {
+                            var keys = ['assignment-task-name','assignment-task-description'];
+
+                            var submission = _.pick(req.body, keys);
+                            var assignmentName = req.body['assignment-name'] == 'null' ? null : req.body['assignment-name'];
+                            // Find the class id then insert event for class
+                            adminDB.getAssignmentByClassCodeAndName(req.body['class-name'], assignmentName, function(err,data){
+                                if(data && data.length > 0) {
+                                    var values = _.values(submission);
+                                    values.unshift(data[0].aId);
+
+                                    adminDB.insertTask(values, function(err,result) {
+                                        directTo(res);
+                                    });
+                                } else {
+                                    req.flash('errorMessage', 'Unable to add task, please check assignment belongs to class.');
+                                    res.end();
+                                }
+                            });
+                        }
+                    });
                 }
             });
         }
@@ -423,28 +491,45 @@ module.exports = function( app ) {
                     err: "a"
                 }
             }));
-        }
-        error = validateTitles(res, route, [name]);
-        if (!error)
-            error = validateDesc(res, route, desc);
-
-        if (!error) {
-            var keys = ['skill-name','skill-description'];
-            var submission = _.pick(req.body, keys);
-            var assignmentName = req.body['assignment-name'] == 'null' ? null : req.body['assignment-name'];
-            // Find the class id then insert event for class
-            adminDB.getAssignmentByClassCodeAndName(req.body['class-name'], assignmentName, function(err,data){
-                if(data && data.length > 0) {
-                    var values = _.values(submission);
-                    values.unshift(data[0].aId);
-                    values.unshift(data[0].classId);
-
-                    adminDB.insertSkill(values, function(err,result) {
-                        directTo(res);
-                    });
+        } else {
+            validateTitles(res, route, [name], function(err, route) {
+                if (err) {
+                    directTo(res, url.format({
+                        pathname: route,
+                        query: {
+                            err: 'title'
+                        }
+                    }));
                 } else {
-                    req.flash('errorMessage', 'Unable to add skill, please check assignment belongs to class.');
-                    res.end();
+                    validateDesc(res, route, desc, function(err2, route) {
+                        if (err2) {
+                            directTo(res, url.format({
+                                pathname: route,
+                                query: {
+                                    err: 'desc'
+                                }
+                            }));
+                        } else {
+                            var keys = ['skill-name','skill-description'];
+                            var submission = _.pick(req.body, keys);
+                            var assignmentName = req.body['assignment-name'] == 'null' ? null : req.body['assignment-name'];
+                            // Find the class id then insert event for class
+                            adminDB.getAssignmentByClassCodeAndName(req.body['class-name'], assignmentName, function(err,data){
+                                if(data && data.length > 0) {
+                                    var values = _.values(submission);
+                                    values.unshift(data[0].aId);
+                                    values.unshift(data[0].classId);
+
+                                    adminDB.insertSkill(values, function(err,result) {
+                                        directTo(res);
+                                    });
+                                } else {
+                                    req.flash('errorMessage', 'Unable to add skill, please check assignment belongs to class.');
+                                    res.end();
+                                }
+                            });
+                        }
+                    });
                 }
             });
         }
