cd241627414bf7c6b6415ec422060c48aa6a95bd
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2018-06-11 16:17:52 -0400

    Mostly finished refactoring - now adding DB store backend

--- ifs/app/components/Survey/routes/index.js
+++ ifs/app/components/Survey/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = (app, iosocket) => {
- // Admin related routes
+ // Admin related routes - keeping this for legacy purposes atm, might remove later
  require('./surveyBuildRoutes.js')(app);
  // General survey routes
  require('./surveyRoutes.js')(app, iosocket);
--- ifs/app/components/Survey/routes/surveyBuildRoutes.js
+++ ifs/app/components/Survey/routes/surveyBuildRoutes.js
@@ -1,184 +1,21 @@
-var path = require('path');
-var fs = require('fs');
-var async = require('async');
-var _ = require('lodash');
-
 const componentPath = path.join(__components,"Survey");
-
-var db = require( __configs + 'database');
-var viewPath = path.join( __components, 'Survey/views/');
-var Logger = require( __configs + "loggingConfig");
-var Survey = require( __components + "Survey/models/Survey");
-var Question = require( __components + "Survey/models/Question")
-var Errors = require(__components + "Errors/errors");
-
-var SurveyManager = require( __components + "Survey/helpers/surveyManager");
-var SurveyBuilder = require( __components + "Survey/helpers/surveyBuilder");
-var SurveyPreferences = require( __components + "Survey/models/SurveyPreferences");
-var SurveyResponse = require(__components + "Survey/surveyResponse");
-
-const staticSurvey = require(path.join(componentPath, 'helpers/staticSurvey'));
 const surveyBuildController = require(path.join(componentPath, 'controllers/surveyBuildController'));
 
 module.exports = function (app) {
 
-
-
     app.get('/createSurveys', surveyBuildController.createSurveys);
 
-    /**
-     * Generate an all matrix survey for a set of questions, read from **surveyQuestions**.json
-     * Puts a single page with all likert/matrix type questions in.
-     * Use other functions to format. This function doesn't overwrite for safety.
-     * @param  {[type]} req  [description]
-     * @param  {[type]} res) {                   var allData [description]
-     * @return {[type]}      [description]
-     */
-    app.get( '/generateMatrixSurvey:n', function(req,res) {
-        var allData = getStaticSurveyData();
+    app.get( '/generateMatrixSurvey:n', surveyBuildController.generateMatrixSurvey);
 
-        var allSurveys = getStaticSurveyData();
-        var i = Math.max(0, Math.min(req.params.n,allSurveys.length-1));
+    app.get( '/buildDefaultSurvey:n', surveyBuildController.buildDefaultSurvey);
 
-        // Survey N becomes the default
-        var [ surveyName, surveyAuthors,surveyTitle, numQuestions, surveyField, freq, surveyFiles, surveyQuestions] = allSurveys[i];
+    app.get('/surveySec/:surveyName/:low/:high/:questionsPerPage?/:splitQuestionTypes?', surveyBuildController.getSurveyWithOptions);
 
-        if(fs.existsSync(surveyFiles) ){
-            console.log("SURVEY FILE EXISTS, will not overwrite, please remove the local file.");
-            res.end();
-            return;
-        }
-
-        Survey.getSurvey(surveyName, function(err,data) {
-             if( err )
-                Logger.error(err);
-            else {
-                if( data.length >= 1){
-                    var survey = data[0];
-                    console.log( surveyQuestions);
-                    var surveyFile = surveyQuestions;
-                    
-                    fs.readFile(surveyFile, "utf-8", function(err,fileData){
-                        if(err)
-                            console.log("Can't read file:",surveyFile);
-                        else {
-                            var jsonData = JSON.parse(fileData);
-                            // For each question insert
-                            if(jsonData['QuestionText']){
-                                
-                                // Build default Survey and write to file, specified in DB.
-                                var s = SurveyBuilder.buildDefaultMatrixSurvey(data[0], jsonData['QuestionText']);
-                                s = JSON.stringify(s);
-                                fs.writeFileSync(String("./" + data[0].fullSurveyFile), s, 'utf-8');
-                                res.end();
-                            }
-                        }
-                    });
-                }
-            }
-        });
-
-    });
-
-    /**
-     * This function is for testing, it will build  and store in DB whichever survey with default params
-     * Default params are English and Matrix Question Type
-     * Note Questions are placed in separted json files, so those are loaded here as well.
-     *     There is a different function to create Surveys
-     * @param  {[type]} req  [description]
-     * @param  {Array}  res) {                   var surveyNames [description]
-     * @return {[type]}      [description]
-     */
-    app.get( '/buildDefaultSurvey:n', function(req,res) {
-
-        var allSurveys = getStaticSurveyData();
-        var i = Math.max(0, Math.min(req.params.n,allSurveys.length-1));
-
-        var [ surveyName, surveyAuthors,surveyTitle, numQs, surveyField, freq, surveyFiles,surveyQuestions] = allSurveys[i];
-
-        Survey.getSurvey(surveyName, function(err,data) {
-             if( err )
-                Logger.error(err);
-            else {
-                if( data.length >= 1){
-                    var survey = data[0];
-                    var surveyFile = surveyQuestions;
-                    
-                    fs.readFile(surveyFile, "utf-8", function(err,fileData){
-                        if(err)
-                            console.log("Can't read file:",surveyFile);
-                        else {
-                            var jsonData = JSON.parse(fileData);
-                            // For each question insert
-                            if(jsonData['QuestionText']){
-                                var dde = [];
-                                for( var j = 0; j < jsonData['QuestionText'].length;j++ ) {
-                                    var defaultData = [
-                                        survey.id, 
-                                        'English',
-                                        j,
-                                        jsonData['QuestionText'][j], 
-                                        "app/components/Survey/SurveyViews/matrixSurveyView.json",
-                                        "matrix",
-                                    ]
-                                    dde.push( defaultData );
-                                }
-                                
-                                async.map( dde,  
-                                    Question.insertQuestion,
-                                    function(err){
-                                        if(err)
-                                            console.log("Insert failed with error", err);
-                                        res.end();
-                                    }
-                                );
-                            }
-                        }
-
-                    });
-               }
-            }
-            res.end();
-        });
-    });
-
-    /**
-     * This route get a survey by name and specific section of questions.
-     * Also allows options parameters to be set.
-     * @param  {[type]} req  [description]
-     * @param  {[type]} res) {                   var surveyName [description]
-     * @return {[type]}      [description]
-     */
-
-    app.get('/surveySec/:surveyName/:low/:high/:questionsPerPage?/:splitQuestionTypes?', function(req,res) {
-        var surveyName = req.params.surveyName;
-        var range = [Math.max(0,Math.min(req.params.low, req.params.high)),Math.max(req.params.low, req.params.high) ];
-
-        var options = SurveyBuilder.setDisplaySurveyOptions(req.params.questionsPerPage, req.params.splitQuestionTypes,range);
-
-        Survey.getSurvey(surveyName, function(err,surveyData) {
-            if( err ) {
-                Logger.error(err);
-            }
-            else if(surveyData.length >= 1 && surveyData[0].surveyName == surveyName)
-            {   
-                SurveyBuilder.getSurveySection(surveyData, options,  function(err,data){
-                    var sqs = JSON.stringify(data);
-                    res.send(sqs);
-                    res.end();
-                });
-            }
-            else {
-                res.end();
-            }
-        });
-    });
-
-    //Helpful little route to create survey prefernces, not sure this will live on.
+    /*//Helpful little route to create survey prefernces, not sure this will live on.
     app.get('/ips', function(req,res) {
         var userId = req.user.id || req.passport.user;
         SurveyBuilder.setSignupSurveyPreferences( userId, function(err,data) {
             res.end();
         });
-    });
+    });*/
 }
