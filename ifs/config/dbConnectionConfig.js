899699a82c789a5de7946ecef221c9334be544c9
Rhys Young <ryoung08@uoguelph.ca>
2018-07-25 11:57:52 -0400

    db-con

--- ifs/config/dbConnectionConfig.js
+++ ifs/config/dbConnectionConfig.js
@@ -11,13 +11,8 @@ module.exports = {
     // modify the database specified below.
     'connection': {
         'host' :                        'localhost',
-<<<<<<< HEAD
         'user' :                        'root',
         'password' :                    'cowscows',
-=======
-        'user' :                        'USER',
-        'password' :                    'PASSWORD',
->>>>>>> a153b373a4bbf7390b88e10e12b57e0ee61acf82
         'connectionLimit':              500
     },
     // database name
