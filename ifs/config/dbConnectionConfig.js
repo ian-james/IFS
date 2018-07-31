489d3daf6529876a7f08a739860f44db76f71638
Rhys Young <ryoung08@uoguelph.ca>
2018-07-31 11:38:10 -0400

    added migrations

--- ifs/config/dbConnectionConfig.js
+++ ifs/config/dbConnectionConfig.js
@@ -16,5 +16,5 @@ module.exports = {
         'connectionLimit':              500
     },
     // database name
-    'database':                         'IFS',
+    'database':                         'IFS2',
 };
