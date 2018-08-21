27eb5f5697e186a4905ce981654f1cd828fa3978
ndomenic <ndomenic@uoguelph.ca>
2018-08-21 14:07:54 -0400

    Removed unnecessary code left over from previous features that were removed from the development branch

--- ifs/app/components/Tool/tool-settings-panel.js
+++ ifs/app/components/Tool/tool-settings-panel.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
+    //Display a previous error message, if it exists
 	var msg = $('#error').text();
+    if (msg.length > 0) {
+        UIkit.notification({message: msg, pos: 'top-center', status: 'danger'});
+    }
+
     var course = $('#course');
     var assign = $('#assign');
     var selectedCourse = "*";
-	if (msg.length > 0) {
-		UIkit.notification({message: msg, pos: 'top-center', status: 'danger'});
-	}
 
     assign.empty();
     course.empty();
@@ -50,43 +52,6 @@ $(document).ready(function(){
             console.log(err);
         }
     })
-
-    // $.ajax({
-    //     type: "post",
-    //     url: '/tool/assignment',
-    //     data: {
-    //         insert: selectedCourse
-    //     },
-    //     success: function (res, status) {
-
-    //         if(res.result.length == 0)
-    //         {
-    //             assign.toggleClass("uk-hidden", true);
-    //             course.toggleClass("uk-hidden", true);
-    //             $('#courseLab').toggleClass("uk-hidden", true);
-    //             $('#assignLab').toggleClass("uk-hidden", true);
-                
-    //         }
-    //         else
-    //         {
-
-    //             var optsStr = "";
-    //             optsStr += "<option value='None'>None</option>"            
-
-    //             for(var i = 0; i < res.result.length; i++)
-    //             {
-    //                 optsStr += "<option value='" + res.result[i] + "'>" + res.result[i] + "</option>";
-    //             }
-
-    //             assign.append( optsStr );
-    //         }     
-    //     },
-    //     error: function(req, err) {
-    //         console.log(err);
-    //     }
-    // })
-
-    
 });
 
 $("#course").change(function() {
@@ -152,9 +117,7 @@ $("#assign").change(function() {
         $('#evaluate').text("Submit Files for " + course.find(":selected").text() + " " + assign.find(":selected").text());
         $('#filePlaceholder').text("Please select files to upload for " + course.find(":selected").text() + " " + assign.find(":selected").text())
 
-    }
-    
-    
+    }        
 });
 
 $("#settingsToggle").click(function() {
@@ -167,23 +130,3 @@ $('#error').bind("DOMSubtreeModified",function(){
 		UIkit.notification({message: msg, pos: 'top-center', status: 'danger'});
 	}
 });
-
-$("#toolPreference").submit(function(event) {
-    var val = event.originalEvent.explicitOriginalTarget.innerHTML;
-
-    $.ajax({
-        type: "post",
-        url:'/tool/preferences',
-        dataType: 'json',
-        data: {
-        	tool: val
-        },
-        success: function (data, textStatus) {
-        	location.reload(true);
-        },
-        error: function (req, err){
-            location.reload(true);
-        }
-    });
-});
-
